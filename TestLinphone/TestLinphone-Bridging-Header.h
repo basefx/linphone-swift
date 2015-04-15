9e8dfeceb3fe82551a65246ca08ac1c1da94d72b
Yevgen Dovgopolyy <basefx@gmail.com>
2015-04-15 15:20:03 +0300

    removed comments

--- TestLinphone/TestLinphone-Bridging-Header.h
+++ TestLinphone/TestLinphone-Bridging-Header.h
@@ -2,10 +2,8 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-// #include "../liblinphone-sdk/apple-darwin/include/linphone/linphonecore.h"
+
 #include "linphone/lpconfig.h"
 #include "linphone/linphonecore.h"
 #include "linphone/linphonecore_utils.h"
 
-
-// /Users/basefx/FP/TestLinphone/liblinphone-sdk/apple-darwin/include/linphone/linphonecore.h
